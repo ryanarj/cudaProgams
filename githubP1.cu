c7561879e1cdc7a370520f8053fa82aa0f9ca9b7
ryanarjun <karshan813@gmail.com>
2018-06-05 10:42:01 -0400

    Change the thread count on the kernal call

--- githubP1.cu
+++ githubP1.cu
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -14,8 +13,12 @@ typedef struct atomdesc {
 } atom;
 
 typedef struct hist_entry{
+	//float min;
+	//float max;
 	unsigned long long d_cnt;   /* need a long long type as the count might be huge */
 } bucket;
+
+
 
 bucket *histogram;		/* list of all buckets in the histogram   */
 long long PDH_acnt;		/* total number of data points            */
@@ -23,8 +26,16 @@ int num_buckets;		/* total number of buckets in the histogram */
 double PDH_res;			/* value of w                             */
 atom *atom_list;		/* list of all data points                */
 
-struct timezone Idunno;	
-struct timeval startTime, endTime;
+
+
+
+// CUDA Error Check
+void checkCudaError(cudaError_t e, const char in[]) {
+	if (e != cudaSuccess) {
+		printf("CUDA Error: %s, %s \n", in, cudaGetErrorString(e));
+		exit(EXIT_FAILURE);
+	}
+}
 
 
 __device__ double
@@ -42,19 +53,6 @@ p2p_distance(atom *a, int ind1, int ind2) {
 }
 
 
-double report_running_time() {
-	long sec_diff, usec_diff;
-	gettimeofday(&endTime, &Idunno);
-	sec_diff = endTime.tv_sec - startTime.tv_sec;
-	usec_diff= endTime.tv_usec-startTime.tv_usec;
-	if(usec_diff < 0) {
-		sec_diff --;
-		usec_diff += 1000000;
-	}
-	printf("Running time for CPU version: %ld.%06ld\n", sec_diff, usec_diff);
-	return (double)(sec_diff*1.0 + usec_diff/1000000.0);
-}
-
 __global__ void 
 PDH_baseline(bucket *histo, atom *atomList, double w, int size) {
 	int i, j, pos;
@@ -63,21 +61,52 @@ PDH_baseline(bucket *histo, atom *atomList, double w, int size) {
 	i = blockIdx.x * blockDim.x + threadIdx.x;
 	j = i + 1;
 
+	
+
 	for (int a = j; a < size; a++) {
+		//printf("i: %d, j: %d \n", i, j);
 		dist = p2p_distance(atomList, i, a);
 		pos = (int) (dist / w);
+
+		//__syncthreads();
+		//histo[pos].d_cnt++;
 		atomicAdd( &histo[pos].d_cnt, 1);
+		//__syncthreads();
 	}
+
+	//printf("\n");
+
 }
 
-void output_histogram(){
+__global__ void
+PDH2D_baseline(bucket *histo, atom *atomList, double w) {
+	int i = (blockIdx.x * blockDim.x) + threadIdx.x;
+	int j = (blockIdx.y * blockDim.y) + threadIdx.y;
+
+	if (i < j) {
+		double dist = p2p_distance(atomList, i, j);
+		int pos = (int) (dist / w);
+
+		histo[pos].d_cnt++;
+
+		printf("%d, %d : %d, %f \n", i, j, pos, dist);
+	}
+
+	__syncthreads();
+
+}
+
+
+
+
+void output_histogram(bucket *histo){
 	int i; 
 	long long total_cnt = 0;
 	for(i=0; i< num_buckets; i++) {
 		if(i%5 == 0) /* we print 5 buckets in a row */
 			printf("\n%02d: ", i);
-		printf("%15lld ", histogram[i].d_cnt);
-		total_cnt += histogram[i].d_cnt;
+		printf("%15lld ", histo[i].d_cnt);
+		total_cnt += histo[i].d_cnt;
 	  	/* we also want to make sure the total distance count is correct */
 		if(i == num_buckets - 1)	
 			printf("\n T:%lld \n", total_cnt);
@@ -90,9 +119,13 @@ int main(int argc, char const *argv[])
 {
 	PDH_acnt = atoi(argv[1]);	// Number of atoms
 	PDH_res = atof(argv[2]);	// Input Distance: W
+
 	num_buckets = (int)(BOX_SIZE * 1.732 / PDH_res) + 1;
+
 	size_t histogramSize = sizeof(bucket)*num_buckets;
 	size_t atomSize = sizeof(atom)*PDH_acnt;
+
+
 	histogram = (bucket *)malloc(histogramSize);
 	atom_list = (atom *)malloc(atomSize);
 
@@ -103,33 +136,51 @@ int main(int argc, char const *argv[])
 		atom_list[i].y_pos = ((double)(rand()) / RAND_MAX) * BOX_SIZE;
 		atom_list[i].z_pos = ((double)(rand()) / RAND_MAX) * BOX_SIZE;
 	}
+
+
+
+
+	// Malloc space on device, copy to device
 	bucket *d_histogram = NULL;
 	atom *d_atom_list = NULL;
 
-	// Allocate memory to GPU arrays and then copy the data from the CPU arrays
-	cudaMalloc((void**) &d_histogram, histogramSize);
-	cudaMalloc((void**) &d_atom_list, atomSize);
-	cudaMemcpy(d_histogram, histogram, histogramSize, cudaMemcpyHostToDevice);
-	cudaMemcpy(d_atom_list, atom_list, atomSize, cudaMemcpyHostToDevice);
+	checkCudaError( cudaMalloc((void**) &d_histogram, histogramSize), 
+		"Malloc Histogram");
+	checkCudaError( cudaMalloc((void**) &d_atom_list, atomSize), 
+		"Malloc Atom List");
 
-	// Launch the kernal and perform calcualtions with the GPU PDH_baseline
+	checkCudaError( cudaMemcpy(d_histogram, histogram, histogramSize, cudaMemcpyHostToDevice), 
+		"Copy histogram to Device");
+	checkCudaError( cudaMemcpy(d_atom_list, atom_list, atomSize, cudaMemcpyHostToDevice), 
+		"Copy atom_list to Device");
+
+
+
+
+	//dim3 threadsPerBlock(256, 256);		// 64 Threads, 2D
+	//dim3 numBlocks( ceil(PDH_acnt / threadsPerBlock.x), ceil(PDH_acnt / threadsPerBlock.y));
+
+	//PDH2D_baseline <<<numBlocks, threadsPerBlock>>>(d_histogram, d_atom_list, PDH_res);
+
 	PDH_baseline <<<ceil(PDH_acnt/32), 32>>> (d_histogram, d_atom_list, PDH_res, PDH_acnt);
-	cudaMemcpy(histogram, d_histogram, histogramSize, cudaMemcpyDeviceToHost);
 
-	/* check the total running time */ 
-	report_running_time();
+	checkCudaError(cudaGetLastError(), "Checking Last Error, Kernel Launch");
 
-	// Print the histogram
-	output_histogram();
+	checkCudaError( cudaMemcpy(histogram, d_histogram, histogramSize, cudaMemcpyDeviceToHost),
+		"Copy device histogram to host");
 
-	// Free the GPU(device) and the CPU(host) arrays
-	cudaFree(d_histogram);
-	cudaFree(d_atom_list);
+
+	output_histogram(histogram);
+
+
+	checkCudaError(cudaFree(d_histogram), "Free device histogram");
+	checkCudaError(cudaFree(d_atom_list), "Free device atom_list");
+
 	free(histogram);
 	free(atom_list);
 
+	checkCudaError(cudaDeviceReset(), "Device reset");
+
+
 	return 0;
 }
-
-
-
